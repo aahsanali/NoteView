8d6e9b670d77613049d17c8c5aa9d80637d6c48e
Dusty <cythbgy@gmail.com>
2015-05-26 17:28:11 +0800

    fix for iOS 8.3

--- Classes/NoteView.m
+++ Classes/NoteView.m
@@ -7,56 +7,49 @@
 //
 
 #import "NoteView.h"
-
+#import <CoreGraphics/CoreGraphics.h>
 
 @implementation NoteView
 
 - (id)initWithFrame:(CGRect)frame {
-	
+    
     self = [super initWithFrame:frame];
     if (self) {
-		[self setBackgroundColor:[UIColor whiteColor]];
+        [self setBackgroundColor:[UIColor whiteColor]];
         self.font = [UIFont fontWithName:@"TimesNewRomanPS-BoldMT" size:12];
-        self.delegate = self;
     }
     return self;
 }
 
 - (void)drawRect:(CGRect)rect {
-	
-    //Get the current drawing context   
+    
+    //Get the current drawing context
     CGContextRef context = UIGraphicsGetCurrentContext();
     
     //Set the line color and width
     CGContextSetStrokeColorWithColor(context, [UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:0.2f].CGColor);
-    CGContextSetLineWidth(context, 1.0f);
-    
+    CGFloat lengths[] = {5.f,5.f};
+    CGContextSetLineDash(context, 0, lengths, 2);
     
     //Start a new Path
     CGContextBeginPath(context);
-	
+    
     //Find the number of lines in our textView + add a bit more height to draw lines in the empty part of the view
-    NSUInteger numberOfLines = (self.contentSize.height + self.bounds.size.height) / self.font.leading;
-	
+    NSUInteger numberOfLines = (self.contentSize.height + self.bounds.size.height) / self.font.lineHeight;
+    
     //Set the line offset from the baseline. (I'm sure there's a concrete way to calculate this.)
     CGFloat baselineOffset = 6.0f;
-	
+    
     //iterate over numberOfLines and draw each line
-    for (NSUInteger x = 1; x < numberOfLines; x++) {
-        
+    for (int x = 1; x < numberOfLines; x++) {
         //0.5f offset lines up line with pixel boundary
         CGContextMoveToPoint(context, self.bounds.origin.x+10, self.font.leading*x + 0.5f + baselineOffset);
         CGContextAddLineToPoint(context, self.bounds.size.width-10, self.font.leading*x + 0.5f + baselineOffset);
     }
-	
+    
     //Close our Path and Stroke (draw) it
     CGContextClosePath(context);
     CGContextStrokePath(context);
 }
-
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
-  [self setNeedsDisplay];
-}
-
 @end
+
